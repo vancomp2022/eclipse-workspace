708aa8ece67f4da311658a4fba95aa9dfa96e0ab
Vanessa Soares de Morais <vanessamoraesvsm@gmail.com>
2022-04-09 23:23:28 -0300

    Initial commit

---
+++ Project_Program/src/Package_Program/Program.java
@@ -0,0 +1,9 @@
+package Package_Program;
+
+public class Program {
+	public static void main(String[] args) {
+		System.out.println("ola mundo");
+	}
+}
+
+
---
+++ sintaxe-variaveis-e-fluxo2/src/Package_Program/TestaVariaveis.java
@@ -0,0 +1,16 @@
+package Package_Program;
+
+public class TestaVariaveis {
+	public static void main() {
+		System.out.println("ola novo teste");
+		
+		int idade;
+		idade = (int) 37.5;
+		
+		double salario = 1270.50;
+		int valor = (int) salario;
+		System.out.println(valor);
+		
+	}
+
+}
